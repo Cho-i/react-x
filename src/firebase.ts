137b164bbbcde8288b40f4fef62f548c05db0e27
이초이 <choi1223@hunet.co.kr>
2024-03-21 16:05:42 +0900

    tweets

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuOEdfZEBtCRh23TbYjXeoJq_uUEMq_G8",
@@ -11,4 +13,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,27 @@
-import { auth } from '../firebase';
+import styled from 'styled-components';
+import PostTweetForm from '../components/post-tweet-form';
+import Timeline from '../components/timeline';
+
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  grid-template-rows: 1fr 5fr;
+  &::-webkit-scrollbar {
+    width: 10px;
+    background-color: transparent;
+    &-thumb {
+      background-color: #dddddd;
+      border-radius: 10px;
+    }
+  }
+`;
 
 export default function Home() {
-  const logOut = () => {
-    auth.signOut();
-  };
   return (
-    <>
-      <div>
-        <button onClick={logOut}>Log Out</button>
-      </div>
-    </>
+    <Wrapper>
+      <PostTweetForm />
+      <Timeline />
+    </Wrapper>
   );
 }
