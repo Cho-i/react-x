a05688470404956c75b25301df635968802dda45
이초이 <choi1223@hunet.co.kr>
2024-03-27 15:27:14 +0900

    test

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,16 +5,7 @@ import Timeline from '../components/timeline';
 const Wrapper = styled.div`
   display: grid;
   gap: 50px;
-  overflow-y: scroll;
   grid-template-rows: 1fr 5fr;
-  &::-webkit-scrollbar {
-    width: 10px;
-    background-color: transparent;
-    &-thumb {
-      background-color: #dddddd;
-      border-radius: 10px;
-    }
-  }
 `;
 
 export default function Home() {
