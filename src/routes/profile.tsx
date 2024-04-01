5dbb9c91c20dac6ebb1480ef8575bfd49dc75d53
이초이 <choi1223@hunet.co.kr>
2024-04-01 15:00:55 +0900

    profile

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,11 @@
 import { styled } from 'styled-components';
-import { auth, storage } from '../firebase';
-import { useState, ChangeEvent } from 'react';
+import { auth, db, storage } from '../firebase';
+import { useState, ChangeEvent, useEffect } from 'react';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { updateProfile } from 'firebase/auth';
+import { ITweet } from '../components/timeline';
+import { collection, getDocs, limit, orderBy, query, where } from 'firebase/firestore';
+import Tweet from '../components/tweet';
 
 const Wrapper = styled.div`
   display: flex;
@@ -35,9 +38,20 @@ const Name = styled.span`
   font-size: 22px;
 `;
 
+const Tweets = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 10px;
+`;
+
 export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [name, setName] = useState(user?.displayName ?? 'Anonymous');
+  const [edit, setEdit] = useState(false);
+
   const onAvatarChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (!user) return;
@@ -49,6 +63,38 @@ export default function Profile() {
       setAvatar(avatarUrl);
       await updateProfile(user, { photoURL: avatarUrl });
     }
+  };
+  const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, 'tweets'),
+      where('userId', '==', user?.uid),
+      orderBy('createdAt', 'desc'),
+      limit(25),
+    );
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+  const onNameChange = async () => {
+    if (!user) return;
+    if (edit) {
+      await updateProfile(user, { displayName: name });
+    }
+    setEdit(!edit);
   };
 
   return (
@@ -68,7 +114,17 @@ export default function Profile() {
         )}
       </AvatarUpload>
       <AvatarInput onChange={onAvatarChange} id="avatar" type="file" accept="image/*" />
-      <Name>{user?.displayName ?? 'Anonymous'}</Name>
+      {edit ? (
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      ) : (
+        <Name>{name}</Name>
+      )}
+      <button onClick={onNameChange}>{edit ? 'Save' : 'Change Name'}</button>
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
     </Wrapper>
   );
 }
